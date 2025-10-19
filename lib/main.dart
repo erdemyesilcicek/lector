a524b67dac003ad20fceea645ca7f329711290fc
erdemyesilcicek <erdmyslcck9@outlook.com>
2025-10-19 14:21:57 +0300

    feat: Integrate Firebase into the project

--- lib/main.dart
+++ lib/main.dart
@@ -1,12 +1,21 @@
+// lib/main.dart
+
 import 'package:flutter/material.dart';
 import 'package:lector/features/navigation_screen.dart';
+import 'package:firebase_core/firebase_core.dart';
+import 'firebase_options.dart';
 
-// Uygulamanın başlangıç noktası
-void main() {
+// main fonksiyonunu async olarak güncelledik
+void main() async {
+  // Flutter'ın hazır olduğundan emin oluyoruz
+  WidgetsFlutterBinding.ensureInitialized();
+  // Firebase'i başlatıyoruz
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
   runApp(const LectorApp());
 }
 
-// Uygulamanın ana iskeleti
 class LectorApp extends StatelessWidget {
   const LectorApp({super.key});
 
